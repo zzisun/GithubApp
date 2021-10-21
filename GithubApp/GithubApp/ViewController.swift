c3b0ea258c95aef7a96daeb73f693ecdeb8a84d2
zzisun <jisunkim98@gmail.com>
2021-10-21 21:44:48 +0900

    feat: #5 SwiftLint 적용

---
+++ GithubApp/GithubApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GithubApp
+//
+//  Created by 김지선 on 2021/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
