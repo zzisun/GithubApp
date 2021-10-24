d100e7849638fa9899f2e844c90531f1f1757771
zzisun <jisunkim98@gmail.com>
2021-10-24 14:29:29 +0900

    add: #2 navigationController embed

--- GithubApp/GithubApp/Presentation/HomeViewController.swift
+++ GithubApp/GithubApp/Presentation/HomeViewController.swift
@@ -11,10 +11,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let networkManager = NetworkManager()
-//        networkManager.search(query: "WorldAfterCapital", decodingType: Results.self) { response in
-//            print(response)
-//        }
+        
         let searchController = UISearchController(searchResultsController: nil)
         
         searchController.searchBar.placeholder = "Search Repository"
@@ -27,55 +24,3 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .blue
     }
 }
-
-//class ViewController: UIViewController {
-//    let tableView: UITableView = UITableView()
-//    var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "The Chainsmokers", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone" ]
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.setupSearchController()
-//        self.setupTableView()
-//
-//    }
-//    func setupSearchController() {
-//        let searchController = UISearchController(searchResultsController: nil)
-//        searchController.searchBar.placeholder = "Search Dj"
-//        searchController.hidesNavigationBarDuringPresentation = false
-//        self.navigationItem.searchController = searchController
-//        self.navigationItem.title = "Search"
-//        self.navigationItem.hidesSearchBarWhenScrolling = false
-//
-//    }
-//    func setupTableView() {
-//        self.tableView.delegate = self
-//        self.tableView.dataSource = self
-//        self.view.addSubview(self.tableView)
-//
-//        self.tableView.translatesAutoresizingMaskIntoConstraints = false
-//        self.view.addConstraint(NSLayoutConstraint(item: self.tableView,
-//                                                           attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top,
-//                                                           multiplier: 1.0, constant: 0))
-//        self.view.addConstraint(NSLayoutConstraint(item: self.tableView,
-//                                                           attribute: .bottom, relatedBy: .equal, toItem: self.view,
-//                                                           attribute: .bottom, multiplier: 1.0, constant: 0))
-//        self.view.addConstraint(NSLayoutConstraint(item: self.tableView,
-//                                                           attribute: .leading, relatedBy: .equal, toItem: self.view,
-//                                                           attribute: .leading, multiplier: 1.0, constant: 0))
-//        self.view.addConstraint(NSLayoutConstraint(item: self.tableView,
-//                                                           attribute: .trailing, relatedBy: .equal, toItem: self.view,
-//                                                           attribute: .trailing, multiplier: 1.0, constant: 0))
-//    }
-//
-//}
-//extension ViewController: UITableViewDelegate, UITableViewDataSource {
-//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return self.arr.count
-//
-//    }
-//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = UITableViewCell()
-//        cell.textLabel?.text = self.arr[indexPath.row]
-//        return cell
-//    }
-//}
-//
