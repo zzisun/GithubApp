2abd3fc80d007796e077f9fe853ed74b255acdd6
zzisun <jisunkim98@gmail.com>
2021-11-01 11:03:27 +0900

    feat: #10 RepositoryCell안에서 attribute 설정하도록 변경

--- GithubApp/GithubApp/Presentation/View/RepositoryCell.swift
+++ GithubApp/GithubApp/Presentation/View/RepositoryCell.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import RxSwift
 
 final class RepositoryCell: UITableViewCell {
     static let id = "RepositoryCell"
+    private var disposeBag = DisposeBag()
 
     @IBOutlet weak var ownerImageView: UIImageView!
     @IBOutlet weak var ownerNameLabel: UILabel!
@@ -21,8 +23,26 @@ final class RepositoryCell: UITableViewCell {
         super.awakeFromNib()
     }
     
-    static func starCount(with starCount: Int?) -> String {
+    private func starCount(with starCount: Int?) -> String {
         guard let starCount = starCount else {return "?"}
         return "\(starCount)"
+    }
+    
+    func configure(repository: Repository) {
+        // cell이 재사용되다보니 스크롤 하면서 이미지가 해당하는 위치에 표시되지 않는 오류가 발생할 수 있다.
+        // 해결방법: 이미지 url과함께 이미지 id를 저장시킨 후 cell이 표시하는 repo의 id와 같은지 표시
+        // 화면이 넘어가면 이미지로드를 cancel하는 경우
+        ImageLoadManager.load(from: repository.owner.avatarURL)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { image in
+                self.ownerImageView.image = image
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.ownerNameLabel.text = repository.owner.name
+        self.repositoryNameLabel.text = repository.name
+        self.repositoryDescriptionLabel.text = repository.description
+        self.starCountLabel.text = starCount(with: repository.starCount)
+        self.languageLabel.text = repository.language
     }
 }
--- GithubApp/GithubApp/Presentation/ViewController/ResultViewController.swift
+++ GithubApp/GithubApp/Presentation/ViewController/ResultViewController.swift
@@ -90,22 +90,7 @@ final class ResultViewController: UIViewController {
         viewModel.repositories
             .bind(to: tableView.rx.items(cellIdentifier: RepositoryCell.id,
                                          cellType: RepositoryCell.self)) { _, repository, cell in
-                // cell이 재사용되다보니 스크롤 하면서 이미지가 해당하는 위치에 표시되지 않는 오류가 발생할 수 있다.
-                // 해결방법: 이미지 url과함께 이미지 id를 저장시킨 후 cell이 표시하는 repo의 id와 같은지 표시
-                // 화면이 넘어가면 이미지로드를 cancel하는 경우
-                ImageLoadManager.load(from: repository.owner.avatarURL)
-                    .observe(on: MainScheduler.instance)
-                    .subscribe(onNext: { image in
-                        cell.ownerImageView.image = image
-                    })
-                    .disposed(by: self.disposeBag)
-                
-                // cell에 repository넘겨서 처리하기
-                cell.ownerNameLabel.text = repository.owner.name
-                cell.repositoryNameLabel.text = repository.name
-                cell.repositoryDescriptionLabel.text = repository.description
-                cell.starCountLabel.text = RepositoryCell.starCount(with: repository.starCount)
-                cell.languageLabel.text = repository.language
+                cell.configure(repository: repository)
             }.disposed(by: disposeBag)
     }
 }
